61bd9f6f3b6c5e7d3c1f26ec2b28375e38ab1514
anthonyfc9101 <anthonyfc9101@gmail.com>
2020-09-25 14:24:37 -0400

    added reviews and deployment

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,16 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+const prodctSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   brand: { type: String, required: true },
@@ -10,8 +20,9 @@ const productSchema = new mongoose.Schema({
   description: { type: String, required: true },
   rating: { type: Number, default: 0, required: true },
   numReviews: { type: Number, default: 0, required: true },
+  reviews: [reviewSchema],
 });
 
-const productModel = mongoose.model("Product", productSchema);
+const productModel = mongoose.model('Product', prodctSchema);
 
 export default productModel;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
-import{BrowserRouter, Route, Link} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
@@ -12,60 +12,82 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import OrdersScreen from './screens/OrdersScreen';
+
 function App() {
-    const userSignin = useSelector(state=>state.userSignin);
-    const {userInfo} = userSignin;
-  const openMenu=()=>{document.querySelector(".sidebar").classList.add("open")};
-  let closeMenu=()=>{document.querySelector(".sidebar").classList.remove("open")};
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const openMenu = () => {
+    document.querySelector('.sidebar').classList.add('open');
+  };
+  const closeMenu = () => {
+    document.querySelector('.sidebar').classList.remove('open');
+  };
   return (
-      <BrowserRouter>
-<div className="grid-contain">
-            <header className="header">
-                <div className="siteLogo">
-                    <button onClick= {openMenu} >&#9776;</button>
-                    <Link to="/">New World Bounty's</Link>
-                </div>
-
-                <div className="headerLinks">
-                <Link to="/cart">cart</Link>
-                    {
-                        userInfo ? <Link to='/profile'>{userInfo.name}</Link>:
-                        <Link to="/signin">Sign In</Link>
-
-                    }
-                </div>
-            </header>
-            <aside className="sidebar">
-                <h3>shopping catagories</h3>
-                <button className="sidebarCloseButton" onClick={closeMenu}>X</button>
-                <ul>
-                    <li>
-                        <a href="/">Pirates</a>
-                    </li>
-                    <li>
-                        <a href="/">Marines</a>
-                    </li>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">OnePieceBounty</Link>
+          </div>
+          <div className="header-links">
+          <Link to="/cart">Cart</Link>
+            {userInfo ? (
+              <Link to="/profile">{userInfo.name}</Link>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#">Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
                 </ul>
-                
-            </aside>
-            <main className="main">
-                <div className="Content">
-                <Route path="/products" component={ProductsScreen} />
-                <Route path="/shipping" component={ShippingScreen} />
+              </div>
+            )}
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul className="categories">
+            <li>
+              <Link to="/category/Pirates">Pirates</Link>
+            </li>
+
+            <li>
+              <Link to="/category/Marines">Marines</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/orders" component={OrdersScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
             <Route path="/payment" component={PaymentScreen} />
             <Route path="/placeorder" component={PlaceOrderScreen} />
-            <Route path="/order/:id" component={OrderScreen} />
-                <Route path="/signin" component={SigninScreen} />
-                <Route path="/register" component={RegisterScreen} />
-                <Route path="/product/:id" component={ProductScreen} />
-                <Route path="/cart/:id?" component={CartScreen} />
-                <Route path="/" exact={true} component={HomeScreen} />
-
-                </div>
-            </main>
-            <footer className="footer">ALL RIGHTS RESEVED</footer>
-        </div>
-        </BrowserRouter>
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/category/:id" component={HomeScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
