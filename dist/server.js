410085e002c4ae23b5ece53a0e6df211bcd12b0d
anthonyfc9101 <anthonyfc9101@gmail.com>
2020-09-25 14:38:31 -0400

    added reviews and deployment

--- dist/server.js
+++ dist/server.js
@@ -2,8 +2,6 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-var _dotenv = _interopRequireDefault(require("dotenv"));
-
 var _config = _interopRequireDefault(require("./config"));
 
 var _mongoose = _interopRequireDefault(require("mongoose"));
@@ -17,8 +15,6 @@ var _productRoute = _interopRequireDefault(require("./routes/productRoute"));
 var _orderRoute = _interopRequireDefault(require("./routes/orderRoute"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-_dotenv["default"].config();
 
 var mongodbUrl = _config["default"].MONGODB_URL;
 
